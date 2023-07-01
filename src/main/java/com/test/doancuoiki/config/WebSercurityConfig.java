d3183a55c7b984b6f66b31ce73aeff457662647c
Dinh Dang Kiem <dingdang4602@gmail.com>
2023-07-01 21:51:26 +0700

    lan3

--- src/main/java/com/test/doancuoiki/config/WebSercurityConfig.java
+++ src/main/java/com/test/doancuoiki/config/WebSercurityConfig.java
@@ -5,6 +5,7 @@ import com.test.doancuoiki.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,6 +33,7 @@ public class WebSercurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Bean
+    @Lazy
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
